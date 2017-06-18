7ce53a4d0c76ad54f8b344eb71e3160122e69610
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-06-18 15:09:12 +0200

    more progress

--- dream-parse.js
+++ dream-parse.js
@@ -2,36 +2,84 @@ const fs = require('fs');
 //const {parse, SyntaxError} = require('./dream-parser');
 const {parse} = require('./dream-parser');
 
+function evalExpr(expression) {
+  console.log('dream-parse.js evalExpr: expression =', expression);
+  const type = typeof expression;
+  if (type === 'string') return `'${expression}'`;
+  if (type !== 'object') return expression;
+
+  const {args, kind, name, params} = expression;
+
+  switch (kind) {
+    case 'call':
+      return `${name}(${args.join(', ')});`;
+
+    case 'function': {
+      const paramNames = params.map(p => p[0].name);
+      const beforeArrow = paramNames.length === 1 ?
+        paramNames[0] : `(${paramNames.join(', ')})`;
+      const afterArrow = evalExpr(expression.expression);
+      return `${beforeArrow} => ${afterArrow}`;
+    }
+
+    case 'write':
+      return `console.log(${evalExpr(expression.expression)});`;
+
+    default:
+      return undefined;
+  }
+}
+
+function toJs(node) {
+  if (!node) return;
+  console.log('dream-parse.js toJs: node =', node);
+  const {args, expression, kind, name, value} = node;
+
+  switch (kind) {
+    case 'assignment':
+      return `const ${name} = ${evalExpr(expression)}`;
+    case 'blank line':
+      return '';
+    case 'call': {
+      console.log('dream-parse.js toJs: args =', args);
+      const argValues = args.map(evalExpr);
+      return `${name}(${argValues.join(', ')});`;
+    }
+    case 'comment':
+      return '//' + value;
+    case 'function':
+      return `() => ${expression}`;
+    case 'write':
+      return `console.log(${evalExpr(expression)});`;
+  }
+}
+
 const [,, inPath, outPath] = process.argv;
 console.log('creating', outPath);
 console.log('parsing', inPath, 'which contains');
 
-function toJs(node) {
-  const {type, value} = node;
-  switch (type) {
-    case 'assignment':
-      return `const ${node.name} = ${value}`;
-    case 'comment':
-      return '// ' + value;
-    case 'write':
-      return `console.log(${value});`;
-    default:
-      return '';
-  }
-}
-
 fs.readFile(inPath, (err, buf) => {
   if (err) throw err;
   const text = buf.toString().trim();
-  console.log(text, '\n');
-  const [nodes] = parse(text);
-  console.log('nodes =', nodes);
+  //console.log(text, '\n');
 
-  const ws = fs.createWriteStream(outPath);
-  for (const node of nodes) {
-    const line = toJs(node);
-    console.log(line);
-    ws.write(line + '\n');
+  try {
+    const nodes = parse(text);
+
+    const ws = fs.createWriteStream(outPath);
+    for (const node of nodes) {
+      const line = toJs(node[0]);
+      console.log(line);
+      ws.write(line + '\n');
+    }
+    ws.end();
+  } catch (e) {
+    //console.error('e =', e);
+    const {expected, found, location} = e;
+    const {start} = location;
+    const {column, line} = start;
+    console.error('syntax error on line', line, 'at column', column);
+    console.error('found', found);
+    console.error('expected one of', expected);
   }
-  ws.end();
 });
--- dream.pegjs
+++ dream.pegjs
@@ -1,53 +1,93 @@
 start
-  = (comment / expression / write)* newline?
+  = ((assignment / blankLine / comment / expression) newline?)*
 
 assignment
-  = name:variableName ws '=' ws value:value newline? {
-  //console.log('assignment: name = name');
-  //console.log('assignment: value = value');
-  return {type: 'assignment', name, value};
-}
+  // after =, can have a value or a function!
+  = name:name ws '=' ws expression:expression {
+    console.log('found assignment to', name);
+    console.log('found assignment with expression =', expression);
+    return {kind: 'assignment', name, expression};
+  }
 
-//call = ...
+blankLine
+  = newline {
+    //console.log('found blank line');
+    return {kind: 'blank line'};
+  }
+
+boolean
+  = 'true' / 'false'
+
+call
+  = '(' name:name args:(ws expression)* ')' {
+    console.log('found call to', name);
+    args = args.map(arr => arr[1]);
+    console.log('with arguments', args);
+    return {kind: 'call', name:name, args:args};
+  }
 
 comment
   = singleLineComment
 
-//expression = assignment / call
 expression
-  = assignment
+  = call / function / value / write
+
+function
+  = params:(parameter ws)* '=>' ws expression:expression {
+    console.log('found function with params =', params);
+    console.log('found function with expression =', expression);
+    return {kind: 'function', params, expression};
+  }
 
 integer
-  = first:[1-9] rest:[0-9]+ {
-  const text = first + rest.join('');
-  return parseInt(text, 10);
-}
+  = first:[1-9] rest:[0-9]* {
+    //console.log('found integer');
+    const text = first + rest.join('');
+    return parseInt(text, 10);
+  }
 
-newline = '\n'
-
-singleLineComment
-  = '--' rest:[^\n]* newline? {
-  const commentText = rest.join('');
-  //console.log('singleLineComment: commentText =', commentText);
-  return {
-    type: 'comment',
-    value: commentText
-  };
-}
-
-value
-  = integer
-
-variableName
+name
   = first:[a-z] rest:[a-z0–9]i* {
     const name = first + rest.join('');
-    //console.log('variableName: name =', name);
+    //console.log('found name', name);
     return name;
   }
 
+newline = '\n' {
+  //console.log('found newline');
+}
+
+parameter
+  = name:name ':' type:type {
+    //console.log('found parameter');
+    return {kind: 'parameter', name, type};
+  }
+
+singleLineComment
+  = '--' rest:[^\n]* {
+    //console.log('found singleLineComment');
+    const commentText = rest.join('');
+    return {
+      kind: 'comment',
+      value: commentText
+    };
+  }
+
+string
+  = "'" chars:[^']* "'" {
+    return chars.join('');
+  }
+
+type
+  = 'Bool' / 'Float' / 'Int' / 'String'
+
+value
+  = boolean / integer / string
+
 write
-  = 'write' ws value:variableName newline? {
-    return {type: 'write', value};
+  = 'write' ws expression:expression {
+    console.log('found write with expression', expression);
+    return {kind: 'write', expression};
   }
 
 ws = [ \n]+
